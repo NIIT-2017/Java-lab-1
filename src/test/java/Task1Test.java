13ff261e7c805c3b24996938539b1af955ccbb9d
Koeco97 <komissarovaolesya@yandex.ru>
2020-04-08 23:10:20 +0300

    import tests changed

--- src/test/java/Task1Test.java
+++ src/test/java/Task1Test.java
@@ -1,13 +1,14 @@
-import static org.junit.jupiter.api.Assertions.*;
+  
+import static org.junit.Assert.*;
 
 class Task1Test {
 
-    @org.junit.jupiter.api.Test
+    @org.junit.Test
     void seqCollatz() {
         assertEquals(837799, Task1.seqCollatz());
     }
 
-    @org.junit.jupiter.api.Test
+    @org.junit.Test
     void collatz() {
         assertEquals(525, Task1.collatz(837799));
     }
--- src/test/java/Task2Test.java
+++ src/test/java/Task2Test.java
@@ -1,6 +1,7 @@
-import org.junit.jupiter.api.Test;
+  
+import org.junit.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
 
 class Task2Test {
 
