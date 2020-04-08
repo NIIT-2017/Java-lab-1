7fa9405f12ddedc0c7b88984f4003f479adf4b40
Koeco97 <komissarovaolesya@yandex.ru>
2020-04-08 23:37:55 +0300

    Task2Test and Task6test changed

--- src/test/java/Task2Test.java
+++ src/test/java/Task2Test.java
@@ -7,8 +7,8 @@ public class Task2Test {
 
     @Test
     public void cutNumber() {
-        assertEquals(15.937, Task2.cutNumber(254,3));
-        assertEquals(15.94, Task2.cutNumber(254,2));
-        assertEquals(2.00, Task2.cutNumber(4,2));
+        assertEquals(15.937, Task2.cutNumber(254,3), 0.001);
+        assertEquals(15.94, Task2.cutNumber(254,2), 0.01);
+        assertEquals(2.00, Task2.cutNumber(4,2), 0.01);
     }
 }
--- src/test/java/Task6Test.java
+++ src/test/java/Task6Test.java
@@ -6,11 +6,11 @@ public class Task6Test {
 
     @Test
     public void earthAndRope() {
-        assertEquals(6378100.078125, Task6.earthAndRope(6378.1, 1));
+        assertEquals(6378100.078, Task6.earthAndRope(6378.1, 1), 0.001);
     }
 
     @Test
     public void pool() {
-        assertEquals(72256.63103256523, Task6.pool(1000, 2000, 1, 3));
+        assertEquals(72256.631, Task6.pool(1000, 2000, 1, 3), 0.001);
     }
 }
