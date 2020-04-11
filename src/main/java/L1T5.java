cb1346a64e53c8074cf84b93807d192cad2d5ec6
ViktorGa <57584117+ViktorGa@users.noreply.github.com>
2020-04-11 16:58:13 +0300

    Update L1T5.java

--- src/main/java/L1T5.java
+++ src/main/java/L1T5.java
@@ -1,54 +1,51 @@
-import java.util.Arrays; //uvi, bad primer, nado formirovat' vse chislo postrocno, vizivaya elements massiva
 public class L1T5
 {
-    static String[] zero ={"\n  __ \n |  |\n |  |\n  -- "};
-    static String[] one = {"\n---\n | \n | \n---"};
-    static String[] two = {"\n____\n || \n || \n----"};
-    static String[] three = {"\n_____\n ||| \n ||| \n-----"};
-    static String[] four = {"\n  .  \n / | \n/__| \n   | "};
-    static String[] five = {"\n| F -\n| I  \n  V |\n_ E |"};
-    static String[] six =  {"\n.--.\n|__\n|  |\n|__|"};
-    static String[] seven = {"\n____\n   /\n  | \n  | "};
-    static String[] eight = {"\n8888\n8__8\n8  8\n8888"};
-    static String[] nine = {"\n  N |\n__I |\n  N |\n__E_|"};
+    static String[] zero ={"  __ ", "|  |", "|  |", " -- "};
+    static String[] one = {" ---", " | ", " | ", " ---"};
+    static String[] two = {" ____", " || ", " || ","----"};
+    static String[] three = {" _____", " ||| ", " ||| ","-----"};
+    static String[] four = {"   .  ", " / | ", "/__| ","   | "};
+    static String[] five = {" | F -"," | I  ","  V |","_ E |"};
+    static String[] six =  {" .--."," |__","|  |","|__|"};
+    static String[] seven = {" ____", "  /", " | ","  | "};
+    static String[] eight = {" 8888","8__8"," 8  8","8888"};
+    static String[] nine = {"   N |","__I |", " N |","__E_|"};
+    public static String getOutpotString(String number, int i)
+    {
+        StringBuilder sb = new StringBuilder();
+        for(int j=0; j<number.length(); j++)
+        {
+            if(number.charAt(j) == '0')
+                sb.append(zero[i]);
+            if(number.charAt(j) == '1')
+                sb.append(one[i]);
+            if(number.charAt(j) == '2')
+                sb.append(two[i]);
+            if(number.charAt(j) == '3')
+                sb.append(three[i]);
+            if(number.charAt(j) == '4')
+                sb.append(four[i]);
+            if(number.charAt(j) == '5')
+                sb.append(five[i]);
+            if(number.charAt(j) == '6')
+                sb.append(six[i]);
+            if(number.charAt(j) == '7')
+                sb.append(seven[i]);
+            if(number.charAt(j) == '8')
+                sb.append(eight[i]);
+            if(number.charAt(j) == '9')
+                sb.append(nine[i]);
+        }
+        String result = sb.toString();
+        return result;
+    }
     public static void main(String[] args)
     {
-        String[] input = {"6","4","7"};
-        for(int i = 0; i < input.length; i++)
+        if(args.length == 0)
+            return;
+        for(int i = 0; i<one.length; i++)
         {
-            switch (input[i])
-            {
-                case "0":
-                    System.out.print(Arrays.toString(zero));
-                    break;
-                case "1":
-                    System.out.print(Arrays.toString(one));
-                    break;
-                case "2":
-                    System.out.print(Arrays.toString(two));
-                    break;
-                case "3":
-                    System.out.print(Arrays.toString(three));
-                    break;
-                case "4":
-                    System.out.print(Arrays.toString(four));
-                    break;
-                case "5":
-                    System.out.print(Arrays.toString(five));
-                    break;
-                case "6":
-                    System.out.print(Arrays.toString(six));
-                    break;
-                case "7":
-                    System.out.print(Arrays.toString(seven));
-                    break;
-                case "8":
-                    System.out.print(Arrays.toString(eight));
-                    break;
-                case "9":
-                    System.out.print(Arrays.toString(nine));
-                    break;
-            }
+            System.out.println(getOutpotString(args[0], i));//vivod N-noi stroki
         }
     }
 }
