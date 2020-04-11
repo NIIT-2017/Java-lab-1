ca68b73cb4284d94d6414ea3635987d4953fc451
ViktorGa <57584117+ViktorGa@users.noreply.github.com>
2020-04-11 16:51:20 +0300

    Update L1T3Test.java

--- src/main/java/L1T3Test.java
+++ src/main/java/L1T3Test.java
@@ -4,7 +4,7 @@ class L1T3Test
     @org.junit.jupiter.api.Test
     void zoom()
     {
-        assertArrayEquals(new int[]{1},L1T3.zoom("1"));
+        assertArrayEquals(new int[]{1},L1T3.zoom("1"));//test naoborot,sleva otvet,sprava vvod
         assertArrayEquals(new int[]{4,5,6,7},L1T3.zoom("4-7"));
         assertArrayEquals(new int[]{16,17,18,19,20},L1T3.zoom("16-20"));
     }
