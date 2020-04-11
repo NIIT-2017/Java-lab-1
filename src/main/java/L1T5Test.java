3c1b97689c12e8df25e13ca65c37c5b8a0576472
ViktorGa <57584117+ViktorGa@users.noreply.github.com>
2020-04-11 16:57:04 +0300

    +2 test's

---
+++ src/main/java/L1T5Test.java
@@ -0,0 +1,10 @@
+import org.junit.Assert;
+import org.junit.Test;
+public class L1T5Test
+{
+    @Test
+    public void getOutpotString()
+    {
+        Assert.assertEquals("  __  --- _____", L1T5.getOutpotString("013", 0));
+    }
+}
