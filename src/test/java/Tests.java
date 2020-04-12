02bf7384c9e74e4ff7b69dcd4d2263661eeba5be
EveerDrof <EveerDrof@git.com>
2020-04-12 19:06:38 +0300

    Changed pom

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -3,10 +3,6 @@ import static org.junit.jupiter.api.Assertions.*;
 import HomeworkLab1.*;
 
 public class Tests {
-
-    public static boolean almostEqual(double a, double b, double eps){
-        return Math.abs(a-b)<eps;
-    }
     public  static boolean compareStringsArray(String[] arrA,String[] arrB)
     {
         boolean flag=true;
@@ -23,25 +19,21 @@ public class Tests {
     @Test
     public void TestTask1() throws Exception {
         assertEquals(524,Task1.func());
-
     }
     @Test
     public  void  TestTask2()
     {
-        assertEquals(true,almostEqual(Task2.sqrt(4,0.1),2,0.001));
-
+        assertEquals(2,Task2.sqrt(4,0.1),0.001);
     }
     @Test
     public void TestTask3()
     {
         assertEquals("1,2,4,5,6,7,18,19,20,21",Task3.sequence("1,2,4-7,18-21"));
-
     }
     @Test
     public void TestTask4()
     {
         assertEquals("1-2,4-7,18-21",Task4.convolve("1,2,4,5,6,7,18,19,20,21"));
-
     }
     @Test
     public  void TestTask5() throws Exception
@@ -57,12 +49,12 @@ public class Tests {
     @Test
     public  void TestTask6()
     {
-        assertEquals(1.5915494259388652E-4,Task6.main());
+        assertEquals(1.5915494259388652E-4,Task6.main(),0.001);
 
     }
     @Test
     public  void TestTask7()
     {
-        assertEquals(72256.63103256523,Task7.func());
+        assertEquals(72256.63103256523,Task7.func(),0.001);
     }
 }
