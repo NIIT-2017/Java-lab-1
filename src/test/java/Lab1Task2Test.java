d1dd8d7bb62f980e431ca7fa0acff121568fe5a1
asintsov <57562296+asintsov@users.noreply.github.com>
2020-04-13 14:21:00 +0300

    Update Lab1Task2Test.java

--- src/test/java/Lab1Task2Test.java
+++ src/test/java/Lab1Task2Test.java
@@ -6,14 +6,16 @@ public class Lab1Task2Test {
 
     @Test
     public void sqrt1() {
+        DecimalFormat df1 = new DecimalFormat("#0.00");
         Lab1Task2 task2 = new Lab1Task2();
         String [] args = new String[]{"9.0","0.00000000001"};
-        assertEquals(3.0,task2.sqrt(args));
+        assertEquals("3.00",df1.format(task2.sqrt(args)));
     }
     @Test
     public void sqrt2() {
+        DecimalFormat df1 = new DecimalFormat("#0.00");
         Lab1Task2 task2 = new Lab1Task2();
         String [] args = new String[]{"225.0","0.1"};
-        assertEquals(15.000001132796916,task2.sqrt(args));
+        assertEquals("15.00",df1.format(task2.sqrt(args)));
     }
 }
