5f12358375e7e5f2d0e1830b4c39085e04716900
asintsov <57562296+asintsov@users.noreply.github.com>
2020-04-13 12:56:29 +0300

    Update Lab1Task6Test.java

--- src/test/java/Lab1Task6Test.java
+++ src/test/java/Lab1Task6Test.java
@@ -10,16 +10,16 @@ public class Lab1Task6Test {
     public void main1() {
         DecimalFormat df1 = new DecimalFormat("#0.000");
         KnotAroundEarth knot = new KnotAroundEarth();
-        assertEquals("0,318",
+        assertEquals("0.318",
                 df1.format(knot.getDelta_diameter(6378.1, 1.0 / 1000) * 1000));
     }
     @Test
     public void main2() {
         DecimalFormat df1 = new DecimalFormat("#0.00");
         SwimmingPool pool = new SwimmingPool();
-        assertEquals("21991,15",
+        assertEquals("21991.15",
                 df1.format(pool.getWalkway_cost(3, 1, 1000)));
-        assertEquals("50265,48",
+        assertEquals("50265.48",
                 df1.format(pool.getFence_cost(3, 1, 2000)));
     }
 }
