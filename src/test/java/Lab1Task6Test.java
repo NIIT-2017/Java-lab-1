ab2aec11645a4c4b4678f22c7b9db96220ff0284
asintsov <57562296+asintsov@users.noreply.github.com>
2020-04-13 12:51:21 +0300

    Update Lab1Task6Test.java

--- src/test/java/Lab1Task6Test.java
+++ src/test/java/Lab1Task6Test.java
@@ -1,8 +1,8 @@
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 import java.text.DecimalFormat;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 public class Lab1Task6Test {
 
