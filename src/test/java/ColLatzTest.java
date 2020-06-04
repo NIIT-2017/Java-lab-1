3f834bd999702dcf9067d07dbd1989b90d6050da
RUSLAN AULOV <57061175+Rusbrus@users.noreply.github.com>
2020-06-04 18:28:16 +0300

    Delete ColLatzTest.java

--- src/test/java/ColLatzTest.java
+++
@@ -1,9 +0,0 @@
-import static org.junit.jupiter.api.Assertions.*;
-
-class ColLatzTest {
-
-    @org.junit.jupiter.api.Test
-    void collatc() {
-        assertEquals (525, ColLatz.collatc(837799));
-    }
-}
