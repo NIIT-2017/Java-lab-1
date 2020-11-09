59ab21f88c163b09ed600449fc98a44a4f33eccf
Bear346 <57682132+Bear346@users.noreply.github.com>
2020-11-09 12:46:52 +0300

    Create CircleTest.java

---
+++ src/test/CircleTest.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CircleTest {
+
+    @Test
+    void getRadius() {
+        assertEquals(1.21,1.21,0);
+    }
+
+    @Test
+    void getFerence() {
+        assertEquals(3.75,3.75,0);
+    }
+
+    @Test
+    void getArea() {
+        assertEquals(5.78,5.78,0);
+    }
+}
